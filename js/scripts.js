f6e9505cdcb7a70c7bc1a5ef29cb03fd25d6741d
Kayla Ondracek and Michael Albers <kayla@>
2018-01-16 13:37:33 -0800

    add js to combine 2 arrays into a new one and display to user

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-  
+  var suits = ["spades", "clubs", "hearts", "diamonds"]
+  var ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"]
+
+  var deck = suits.forEach(function(suit){
+    return ranks.forEach(function(rank){
+      return $('#deck').append("<p>" + rank + " of " + suit + "</p>");
+    })
+  });
 });
